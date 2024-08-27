fed08bca5f4e9ee89077b981138a7e7af19a3ca3
Heru-kun <58533039+Herutriana44@users.noreply.github.com>
2024-08-27 14:34:19 +0700

    Upload Tugas Pertemuan 1(Pertemuan 1)

---
+++ Pertemuan 1/MahasiswaBeraksi/src/main/java/com/mahasiswa/mahasiswaberaksi/Mahasiswa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mahasiswa.mahasiswaberaksi;
+
+/**
+ *
+ * @author Fasilkom
+ */
+public class Mahasiswa {
+    
+    void membaca()
+    {
+        System.out.println("Mahasiswa membaca");
+    }
+    
+    void nyontek()
+    {
+        System.out.println("Mahasiswa menyontek");
+    }
+    
+    void modifikasi()
+    {
+        System.out.println("Mahasiswa modifikasi");
+    }
+}
---
+++ Pertemuan 1/MahasiswaBeraksi/src/main/java/com/mahasiswa/mahasiswaberaksi/MahasiswaBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mahasiswa.mahasiswaberaksi;
+
+/**
+ *
+ * @author Fasilkom
+ */
+public class MahasiswaBeraksi {
+
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.membaca();
+        mhs.nyontek();
+        mhs.modifikasi();
+    }
+}
