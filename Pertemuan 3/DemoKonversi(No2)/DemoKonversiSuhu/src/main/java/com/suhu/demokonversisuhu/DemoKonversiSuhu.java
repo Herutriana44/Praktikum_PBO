7c5513433f230a30e8a287be87701190156112d1
Herutriana44 <58533039+Herutriana44@users.noreply.github.com>
2024-09-03 14:54:04 +0700

    Perbaikan Kode Demo Konversi Suhu Untuk Tampilan

--- Pertemuan 3/DemoKonversi(No2)/DemoKonversiSuhu/src/main/java/com/suhu/demokonversisuhu/DemoKonversiSuhu.java
+++ Pertemuan 3/DemoKonversi(No2)/DemoKonversiSuhu/src/main/java/com/suhu/demokonversisuhu/DemoKonversiSuhu.java
@@ -13,6 +13,9 @@ public class DemoKonversiSuhu {
     public static void main(String[] args) {
         KonversiSuhu2 tes2 = new KonversiSuhu2();
         tes2.celcius = 20;
-        System.out.println(tes2.fahrenheitToReamur());
+        System.out.println("Celcius \t:\t" + tes2.celcius);
+        System.out.println("Celcius to Fahrenheit\t:\t" + tes2.celciusToFahrenheit());
+        System.out.println("Celcius to Reamur\t:\t" + tes2.celciusToReamur());
+        System.out.println("Fahrenheit to Reamur\t:\t" + tes2.fahrenheitToReamur());
     }
 }
